7005ea1a50a6f0160ef0ebf557accdc0798d237d
Stephane Nsakala <maduma@pt.lu>
2023-05-26 14:53:15 +0200

    build working

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,13 @@
 #![no_std]
 #![no_main]
 
-use core::panic::PanicInfo;
-use core::arch::asm;
+use core::fmt::Write;
 
 const UART: usize = 0x10000000;
 const THR_EMPTY_AND_LINE_IDLE: u8 = 1 << 6;
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(_info: &core::panic::PanicInfo) -> ! {
     loop{}
 }
 
@@ -20,12 +19,6 @@ fn panic(_info: &PanicInfo) -> ! {
 pub fn console(uart: usize, msg: &str) {
     let base_addr = uart as *mut u8;
     unsafe {
-        core::ptr::write_volatile(base_addr.offset(3), 1 << 7);
-        let tmp2: u8 = core::ptr::read_volatile(base_addr);
-        asm!("mv t2, {}", in(reg) tmp2);
-        let tmp1: u8 = core::ptr::read_volatile(base_addr.offset(3));
-        asm!("mv t1, {}", in(reg) tmp1);
-        core::ptr::write_volatile(base_addr.offset(3), 0);
         let status_addr = base_addr.offset(5);
         for c in msg.bytes() {
             while core::ptr::read_volatile(status_addr) & THR_EMPTY_AND_LINE_IDLE == 0 { }
@@ -34,12 +27,21 @@ pub fn console(uart: usize, msg: &str) {
     }
 }
 
+struct Serial;
+
+impl Write for Serial {
+    fn write_str(&mut self, msg: &str) -> core::fmt::Result {
+        console(UART, msg);
+        Ok(())
+    }
+}
+
 #[no_mangle]
 #[link_section = ".init"]
 pub fn _start() -> ! {
 
     extern "C" {
-        static  _stack_start: u32;
+        static  _stack_start: usize;
     }
 
     type FnPtr = fn() -> !;
@@ -47,20 +49,23 @@ pub fn _start() -> ! {
 
     unsafe{
         let sp = &_stack_start;
-        asm!("csrw mtvec, {}" ,
+        core::arch::asm!("csrw mtvec, {}" ,
             in(reg) th);
-        asm!("mv sp, {}" ,
+        core::arch::asm!("mv sp, {}" ,
             in(reg) sp);
     }
 
-    let msg ="\
+    let _msg ="\
         Hello World!\n\
         How are you men!\n\
         this\tis\tmy\tname\n\
         one\ttime\tis\tthere\n\
         ";
 
-    console(UART, msg);
+    let mut cons = Serial;
+    write!(cons, "Hello {}!", "Stephane").unwrap();
+    
+    //cons.write_str(&_msg).unwrap();
 
     loop {}
 }
