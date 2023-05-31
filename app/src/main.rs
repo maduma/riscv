74333beba21a7304e3a6dbd94f93c6b4267a1424
Stephane Nsakala <maduma@pt.lu>
2023-05-31 11:08:30 +0200

    strange behavior

--- app/src/main.rs
+++ app/src/main.rs
@@ -8,6 +8,7 @@ use core::ptr::read_volatile;
 use core::ptr::write_volatile;
 use core::fmt::Write;
 use core::fmt::Result;
+use core::ptr;
 
 const UART: usize = 0x10000000;
 const THR_EMPTY_AND_LINE_IDLE: u8 = 1 << 6;
@@ -35,33 +36,38 @@ impl Write for Serial {
 #[entry]
 fn main() -> ! {
 
-    let _msg ="\
-        Hello World!\n\
-        How are you men!\n\
-        this\tis\tmy\tname\n\
-        one\ttime\tis\tthere\n\
-        ";
-
-    let mut cons = Serial;
-    write!(cons, "Hello {}!\n", "Stephane").unwrap();
     extern "C" {
         // Boundaries of the .bss section
-        static mut _ebss: u32;
-        static mut _sbss: u32;
+        static _ebss: u32;
+        static _sbss: u32;
 
         // Boundaries of the .data section
-        static mut _edata: u32;
-        static mut _sdata: u32;
+        static _edata: u32;
+        static _sdata: u32;
 
         // Initial values of the .data section (stored in Flash)
         static _sidata: u32;
     }
+
+    let mut cons = Serial;
+
     unsafe {
+        let a = &_sdata as *const u32;
+	let b = &_sidata as *const u32;
+	let c = &_sdata;
+        let d = &_sidata;
         write!(cons, "_sdata {:p}\n", &_sdata).unwrap();
         write!(cons, "_edata {:p}\n", &_edata).unwrap();
         write!(cons, "_sidata {:p}\n", &_sidata).unwrap();
         write!(cons, "_sbss {:p}\n", &_sbss).unwrap();
         write!(cons, "_ebss {:p}\n", &_ebss).unwrap();
+        write!(cons, "{:?}\n", ptr::addr_of!(_sidata)).unwrap();
+        write!(cons, "{:?} {:?} {}\n", ptr::addr_of!(_sdata), ptr::addr_of!(_sidata), ptr::addr_of!(_sdata) == ptr::addr_of!(_sidata)).unwrap();
+        write!(cons, "{:?} {:?}\n", a, b).unwrap();
+        write!(cons, "same1? {}\n", a == b).unwrap();
+        write!(cons, "same2? {}\n", ptr::eq(c, d)).unwrap();
+        write!(cons, "same2? {}\n", ptr::eq(&_sdata, &_sidata)).unwrap();
+        write!(cons, "same3? {}\n", ptr::eq(&_sdata as *const _, &_sidata as *const _)).unwrap();
     }
 
     loop { }
