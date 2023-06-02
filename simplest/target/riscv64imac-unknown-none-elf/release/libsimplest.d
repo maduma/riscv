05baf22dcb598df76d72cab7d7987b09ca14ddc0
Stephane Nsakala <maduma@pt.lu>
2023-06-02 15:14:46 +0200

    simplest initialisation

--- simplest/target/riscv64imac-unknown-none-elf/release/libsimplest.d
+++
@@ -1,1 +0,0 @@
-/home/snsakala/rust-on-riscv/simplest/target/riscv64imac-unknown-none-elf/release/libsimplest.rlib: /home/snsakala/rust-on-riscv/simplest/src/asm.s /home/snsakala/rust-on-riscv/simplest/src/lib.rs
